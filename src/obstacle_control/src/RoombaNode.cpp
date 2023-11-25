d05609ee757609880d6ca8ba2315c37263206927
lorocks <lowell.lobo@hotmail.com>
2023-11-25 18:42:28 -0500

    Fix cpplint and add cppcheck and cpplint to results

--- src/obstacle_control/src/RoombaNode.cpp
+++ src/obstacle_control/src/RoombaNode.cpp
@@ -32,7 +32,7 @@ void Roomba::detect_obstacle(const sensor_msgs::msg::Image& image) {
   auto velocity = geometry_msgs::msg::Twist();
   velocity.linear.x = 0.25;
   velocity.angular.z = 0.0;
-  auto imagedata = (float*)image.data.data();
+  auto imagedata = reinterpret_cast<const float*>(image.data.data());
   for (unsigned int i = 0; i < image.height / 2; i++) {
     for (unsigned int j = (unsigned int)(image.width / 4);
          j < (unsigned int)(image.width * 3 / 4); j++) {
